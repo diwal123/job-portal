642b3f70b57c43ffbd4ab427208cb62dbe64d61b
diwal123 <diwalpyakurel@gmail.com>
2019-08-05 23:16:40 -0500

    Setting up the files for menu bar

---
+++ src/Components/MenuBarComponent/About.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class About extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+    render(){
+        return(
+            <h1>This is us</h1>
+        )
+    }
+}
+
+export default About;
---
+++ src/Components/MenuBarComponent/EmployerLogin.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class EmployerLogin extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+    render(){
+        return(
+            <h1>This is employer login</h1>
+        )
+    }
+}
+
+export default EmployerLogin;
