2e1213a2dea5d65c473279bfa1453e08a42ce453
diwal123 <diwalpyakurel@gmail.com>
2019-08-11 03:34:01 -0500

    get and Post sample set up with component did mount

--- src/Components/MenuBarComponent/Home.js
+++ src/Components/MenuBarComponent/Home.js
@@ -1,15 +1,45 @@
 import React, {Component} from "react";
+import axios from "axios"
 
 class Home extends Component{
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            jobTable: ""
+        };
+
+        this.jobBlock=this.jobBlock.bind(this);
+      }
+
+      jobBlock(jobs){
+          return(
+          <div>
+              <h4>{jobs.Title}</h4>
+              <h6>{jobs.Location}, {jobs.Type},{jobs.Education}, {jobs.Experience}, {jobs.Company}</h6>
+          </div>
+          )
+      }
+
+      componentDidMount() {
+        var self=this;
+        axios.get("http://localhost:3001/jobs")//changed here
+        .then(function (response) {
+          console.log(response.data);
+          console.log(self);
+          self.setState({jobTable: response.data})
+         
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
       }
 
     render(){
         return(
-            <h1>This is Home</h1>
+            <div>
+                {this.state.jobTable?this.state.jobTable.map(item=>this.jobBlock(item)):null}
+            </div>
         )
     }
 }
