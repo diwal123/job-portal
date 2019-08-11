d46b56771b640b131fe288208b57b13f48d211f6
diwal123 <diwalpyakurel@gmail.com>
2019-08-11 02:48:45 -0500

    Setting up the POST sample request

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-const cors = require('cors');
-
-
+const cors = require("cors");
 
 //const db = require("./models/boardModel");
 const db = require("./src/Components/model");
@@ -19,16 +17,20 @@ app.use(cors());
 
 //app.use(express.urlencoded({ extended: true }));
 //app.use(express.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true,
- parameterLimit: 10000,
- limit: 1024 * 1024 * 10
-}));
-app.use(bodyParser.json({
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
+  })
+);
+app.use(
+  bodyParser.json({
     extended: true,
- parameterLimit: 10000,
- limit: 1024 * 1024 * 10
-}));
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
+  })
+);
 
 app.use(express.static("public"));
 
@@ -36,38 +38,43 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/krishna";
 
 mongoose.connect(MONGODB_URI);
 
+app.get("/users", function(req, res) {});
 
-app.get("/users", function(req, res) {
-  
+app.get("/jobs", function(req, res) {
+  db.find({})
+    .then(function(response) {
+      res.json(response);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
+app.post(
+  "/add/:title/:location/:type/:education/:experience/:company",
+  function(req, res) {
+    var newDoc = new db({
+      Title: req.params.title,
+      Location: req.params.location,
+      Type: req.params.type,
+      Education: req.params.education,
+      Experience: req.params.experience,
+      Company: req.params.company
+    });
+    newDoc.save(function(err, book) {
+      if (err) return console.error(err);
+      console.log("Saved");
+    });
 
-
-app.get("/users/:roomId", function(req, res) {
-  
-});
-
-
-app.post("/add/:ID/:jd/:company", function(req, res) {
- 
-  var newDoc= new db({ID: req.params.ID, jobDescription: req.params.jd, company: req.params.company})
-newDoc.save(function (err, book) {
-    if (err) return console.error(err);
-    console.log("Saved");
-  });
-
-  // db.insert({ roomID: req.params.roomID }, {boardID: req.params.boardID} )
-  //   .then(function(response) {
-  //     res.json(response);
-  //   })
-  //   .catch(function(err) {
-  //     res.json(err);
-  //   });
-});
-
-
-
-
+    // db.insert({ roomID: req.params.roomID }, {boardID: req.params.boardID} )
+    //   .then(function(response) {
+    //     res.json(response);
+    //   })
+    //   .catch(function(err) {
+    //     res.json(err);
+    //   });
+  }
+);
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
--- src/Components/model.js
+++ src/Components/model.js
@@ -3,9 +3,28 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var JobTable = new Schema({
-  ID: String,
-  jobDescription:String,
-  company: String 
+  Title: {
+    type: String,
+    required: [true, "Please add Title"]
+  },
+  Location: {
+    type: String,
+    required: [true, "Please add Location"]
+  },
+  Type: {
+    type: String,
+    required: [true, "Please add Type"]
+  },
+  Education: {
+    type: String,
+    required: [true, "Please add Education"]
+  },
+  Experience: {
+    type: Number,
+    required: [true, "Please add Experience"]
+  },
+
+  Company: String
 });
 
 var Table = mongoose.model("Table", JobTable);
